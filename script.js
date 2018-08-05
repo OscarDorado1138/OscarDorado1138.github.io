d610798d05234ffeac8f56359368b614b63dede2
OscarDorado1138 <OscarDorado1138@Berkeley.edu>
2018-08-05 00:54:41 -0700

    Update

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-<!DOCTYPE html>
-
-<!--
+/*
 Copyright 2015 Google Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,21 +12,13 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
+*/
 
-<html>
+"use strict";
 
-<head>
-    <title>Hello, world</title>
-    <link rel="stylesheet" href="/style.css">
-    <script src="/script.js"></script>
-</head>
-
-<body>
-    <h1>Hello, world</h1>
-
-    <button onclick="fetchMessage()">Fetch Message</button>
-    <p id="message">Click on the button to fetch the message.</p>
-</body>
-
-</html>
+function fetchMessage() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "/hello", false);
+    xmlHttp.send(null);
+    document.getElementById("message").innerHTML = xmlHttp.responseText;
+}
